c31472e5a12d96c6469e1cdc8eeb2ce455cec043
Tyler Miller <tylermillerhome@gmail.com>
2016-12-21 10:38:16 -0800

    post to inventory works without transaction

--- routes/inventory.js
+++ routes/inventory.js
@@ -29,8 +29,86 @@ router.get('/inventory', authenticate, (req, res, next) => {
 
 router.post('/inventory', authenticate, (req, res, next) => {
   const { userId } = req.token;
-  const { balance, toolType } = req.body;
-  
+  const { requestedTool } = req.body;
+
+  knex('users')
+    .where('auth_id', userId)
+    .then((row) => {
+      if (!row) {
+        return next('No user found!');
+      }
+      const user = camelizeKeys(row);
+
+      return user;
+    })
+    .then((user) => {
+      knex('tools')
+        .where('tool_name', requestedTool)
+        .then((row) => {
+          if (!row) {
+            return next('No tool found!')
+          }
+          const tool = camelizeKeys(row);
+
+          return tool;
+        })
+        .then((tool) => {
+          if (user.balance >= tool.price) {
+            const newBalance = user.balance - tool.price;
+
+            knex('users')
+              .where('auth_id', userId)
+              .update({
+                balance: newBalance
+              })
+              .then((user) => {
+                const newTool = decamelizeKeys({
+                  toolId: tool.id,
+                  userId: userId
+                });
+
+                const toSend = {
+                  balance: newBalance,
+                  tool: tool
+                };
+
+                knex('tools_users')
+                  .insert(newTool, '*')
+                  .then((row) => {
+                    res.send(toSend);
+                  })
+                  .catch((err) => {
+                    console.error('Insert failed: rolling back.');
+                    console.error(err);
+
+                    knex('users')
+                      .where('auth_id', userId)
+                      .update({
+                        balance: user.balance
+                      })
+                      .then((user) => {
+                        res.send('Rollback success, ')
+                      })
+                      .catch((err) => {
+                        console.error('Rollback Failed');
+                        console.error(err);
+                      });
+                  });
+              })
+              .catch((err) => {
+                console.error(err);
+              });
+          } else {
+            res.send('You cannot afford this tool!');
+          }
+        })
+        .catch((err) => {
+          next(err);
+        });
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
 module.exports = router;
