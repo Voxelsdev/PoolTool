c198ca8e2769517f5dbecd3a8fdf084747f9b5d6
Tyler Miller <tylermillerhome@gmail.com>
2017-01-06 22:00:53 -0800

    socket route progress

--- routes/sockets.js
+++ routes/sockets.js
@@ -33,53 +33,116 @@ router.post('/end', (req, res, next) => {
 
       for (let i = 0; i < toolsUsed.length; i++) {
         knex('tools_users')
+          .select('*').from('tools_users')
           .innerJoin('users', 'users.id', 'tools_users.user_id')
-          .innerJoin('tools', 'tools.id', 'tools_users.tool.id')
+          .innerJoin('tools', 'tools.id', 'tools_users.tool_id')
           .where('users.id', userId)
           .andWhere('tools.id', toolsUsed[i].toolId)
           .andWhere('tools_users.current_durability', '>', 0)
-          .first()
-          .decrement('current_durability', toolsUsed[i].durabilityUsed)
-          .from('tools_users')
-          .then(() => {
-            console.log('tool updated');
+          .then((rows) => {
+            const userTools = camelizeKeys(rows);
+            const newDurability = userTools[0].currentDurability - toolsUsed[i].durabilityUsed;
+
+            if (newDurability > -1) {
+              knex('tools_users')
+                .where('user_id', userId)
+                .andWhere('tool_id', toolsUsed[i].toolId)
+                .andWhere('current_durability', '>', 0)
+                .first()
+                .update({
+                  current_durability: newDurability,
+                })
+                .catch((err) => {
+                  console.log(`Setting user ${userId}'s tool ${toolsUsed[i].toolId} durability to ${newDurability} failed`);
+                  console.error(err);
+                });
+            } else {
+              const durabilitySum = userTools.reduce((inc, e, i, arr) => {
+                return e.currentDurability;
+              }, 0);
+
+              if (toolsUsed[i].durabilityUsed > durabilitySum) {
+                knex('users')
+                  .where('id', userId)
+                  .decrement('balance', reward.toFixed(2))
+                  .then(() => {
+                    knex('users')
+                      .where('id', 1)
+                      .increment('balance', reward.toFixed(2))
+                      .catch((err) => {
+                        console.log(`Incrementing admin balance by ${reward} failed`);
+                        console.error(err);
+                      });
+                  })
+                  .catch((err) => {
+                    console.log(`Decrementing user ${userId}'s balance by ${reward} failed`);
+                    console.error(err);
+                  });
+              } else {
+                const numToolsUsed = Math.floor(toolsUsed[i].durabilityUsed / userTools[1].currentDurability);
+                const remaining = durabilitySum - (numToolsUsed * userTools[1].currentDurability);
+
+                for (let j = 0; j < numToolsUsed; j++) {
+                  knex('tools_users')
+                    .where('user_id', userId)
+                    .andWhere('tool_id', toolsUsed[i].toolId)
+                    .andWhere('current_durability', '>', 0)
+                    .first()
+                    .update({
+                      current_durability: 0,
+                    })
+                    .catch((err) => {
+                      console.log(`Setting user ${userId}'s tool ${toolsUsed[i].toolId} to 0 failed`);
+                      console.error(err);
+                    });
+                }
+
+                if (remaining > 0) {
+                  knex('tools_users')
+                    .where('user_id', userId)
+                    .andWhere('tool_id', toolsUsed[i].toolId)
+                    .andWhere('current_durability', '>', 0)
+                    .first()
+                    .update({
+                      current_durability: usedTools[numToolsUsed].current_durability - remaining,
+                    })
+                    .catch((err) => {
+                      console.log(`Setting user ${userId}'s tool ${toolsUsed[i].toolId} to ${remainder} failed`);
+                      console.error(err);
+                    });
+                }
+              }
+            }
           })
           .catch((err) => {
             console.error(err);
-            rollback = true;
+            knex('tools_users')
+              .innerJoin('users', 'users.id', 'tools_users.user_id')
+              .innerJoin('tools', 'tools.id', 'tools_users.tool.id')
+              .where('users.id', userId)
+              .andWhere('tools.id', toolsUsed[i].toolId)
+              .first()
+              .increment('tools_users.current_durability', toolsUsed[i].durabilityUsed)
+              .then(() => {
+                console.log('tool rolled back');
+
+                knex('users')
+                  .where('id', userId)
+                  .decrement('balance', reward.toFixed(2))
+                  .then(() => {
+                    console.log(`User ${userId} rolled back, user ${userId} recieved ${reward}`);
+                  })
+                  .catch((err) => {
+                    console.log(`User ${userId}'s balance needs to be rolled back by ${reward}`);
+                 });
+              })
+              .catch((err) => {
+                console.log('------------------------')
+                console.log('tool rollback failed at:');
+                console.log(`User: ${userId} -- toolInfo: ${toolsUsed[i]} -- reward missed: ${reward}`)
+                console.error(err);
+              });
           });
-      }
-
-      if (rollback) {
-        for (let i = 0; i < toolsUsed; i++) {
-          knex('tools_users')
-            .innerJoin('users', 'users.id', 'tools_users.user_id')
-            .innerJoin('tools', 'tools.id', 'tools_users.tool.id')
-            .where('users.id', userId)
-            .andWhere('tools.id', toolsUsed[i].toolId)
-            .first()
-            .increment('tools_users.current_durability', toolsUsed[i].durabilityUsed)
-            .then(() => {
-
-              console.log('tool rolled back');
-
-              knex('users')
-                .where('id', userId)
-                .decrement('balance', reward.toFixed(2))
-                .then(() => {
-                  console.log(`User ${userId} rolled back, balanceRolledBack: ${reward}`);
-                })
-                .catch((err) => {
-                  console.log(`User ${userId}'s balance needs to be rolled back by ${reward}`);
-                });
-            })
-            .catch((err) => {
-              console.log('------------------------')
-              console.log('tool rollback failed at:');
-              console.log(`User: ${userId} -- toolInfo: ${toolsUsed[i]} -- reward missed: ${reward}`)
-              console.error(err);
-            });
-        }
       }
 
       res.send(true);
