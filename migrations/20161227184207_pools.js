071f6cf2863c7965708e7ed63a3ebc3a96cf2945
Tyler Miller <tylermillerhome@gmail.com>
2017-02-07 13:40:38 -0800

    styled mining page, made the db easily wipeable

--- migrations/20161227184207_pools.js
+++ migrations/20161227184207_pools.js
@@ -1,7 +1,7 @@
 'use strict'
 
 exports.up = (knex) => {
-  return knex.schema.raw('ALTER TABLE pools ADD COLUMN geog geography(point, 4326)');
+  return knex.schema.raw('CREATE EXTENSION postgis;\nALTER TABLE pools ADD COLUMN geog geography(point, 4326)');
 };
 
 exports.down = (knex) => {
